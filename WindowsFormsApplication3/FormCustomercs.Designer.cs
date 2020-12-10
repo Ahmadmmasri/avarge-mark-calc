7435b817b16caa55f2a578e3e75cb1882661e9c0
Ahmadmmasri <71886488+Ahmadmmasri@users.noreply.github.com>
2020-12-11 00:49:19 +0200

    Add files via upload

---
+++ WindowsFormsApplication3/FormCustomercs.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class FormCustomercs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelInput = new System.Windows.Forms.Label();
+            this.textBoxVal = new System.Windows.Forms.TextBox();
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.textBoxVal4 = new System.Windows.Forms.TextBox();
+            this.GradeLabel = new System.Windows.Forms.Label();
+            this.textBoxval2 = new System.Windows.Forms.TextBox();
+            this.textBoxval3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextBoxval5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TextBoxval6 = new System.Windows.Forms.TextBox();
+            this.Average = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Grade = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LabelInput
+            // 
+            this.LabelInput.AutoSize = true;
+            this.LabelInput.Location = new System.Drawing.Point(6, 31);
+            this.LabelInput.Name = "LabelInput";
+            this.LabelInput.Size = new System.Drawing.Size(37, 13);
+            this.LabelInput.TabIndex = 1;
+            this.LabelInput.Text = "Arabic";
+            // 
+            // textBoxVal
+            // 
+            this.textBoxVal.Location = new System.Drawing.Point(88, 28);
+            this.textBoxVal.Name = "textBoxVal";
+            this.textBoxVal.Size = new System.Drawing.Size(64, 20);
+            this.textBoxVal.TabIndex = 2;
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Location = new System.Drawing.Point(338, 31);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(75, 23);
+            this.CheckButton.TabIndex = 3;
+            this.CheckButton.Text = "Check";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(338, 60);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "Cancel";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // textBoxVal4
+            // 
+            this.textBoxVal4.Location = new System.Drawing.Point(218, 58);
+            this.textBoxVal4.Name = "textBoxVal4";
+            this.textBoxVal4.Size = new System.Drawing.Size(64, 20);
+            this.textBoxVal4.TabIndex = 5;
+            // 
+            // GradeLabel
+            // 
+            this.GradeLabel.AutoSize = true;
+            this.GradeLabel.Location = new System.Drawing.Point(163, 62);
+            this.GradeLabel.Name = "GradeLabel";
+            this.GradeLabel.Size = new System.Drawing.Size(46, 13);
+            this.GradeLabel.TabIndex = 6;
+            this.GradeLabel.Text = "Science";
+            // 
+            // textBoxval2
+            // 
+            this.textBoxval2.Location = new System.Drawing.Point(218, 28);
+            this.textBoxval2.Name = "textBoxval2";
+            this.textBoxval2.Size = new System.Drawing.Size(64, 20);
+            this.textBoxval2.TabIndex = 7;
+            // 
+            // textBoxval3
+            // 
+            this.textBoxval3.Location = new System.Drawing.Point(88, 55);
+            this.textBoxval3.Name = "textBoxval3";
+            this.textBoxval3.Size = new System.Drawing.Size(64, 20);
+            this.textBoxval3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "English";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Math";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Arts";
+            // 
+            // TextBoxval5
+            // 
+            this.TextBoxval5.Location = new System.Drawing.Point(88, 84);
+            this.TextBoxval5.Name = "TextBoxval5";
+            this.TextBoxval5.Size = new System.Drawing.Size(64, 20);
+            this.TextBoxval5.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "your average";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(165, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Sports";
+            // 
+            // TextBoxval6
+            // 
+            this.TextBoxval6.Location = new System.Drawing.Point(218, 87);
+            this.TextBoxval6.Name = "TextBoxval6";
+            this.TextBoxval6.Size = new System.Drawing.Size(64, 20);
+            this.TextBoxval6.TabIndex = 16;
+            // 
+            // Average
+            // 
+            this.Average.Location = new System.Drawing.Point(88, 158);
+            this.Average.Name = "Average";
+            this.Average.Size = new System.Drawing.Size(64, 20);
+            this.Average.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(167, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Grade";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Grade
+            // 
+            this.Grade.Location = new System.Drawing.Point(218, 157);
+            this.Grade.Name = "Grade";
+            this.Grade.Size = new System.Drawing.Size(118, 20);
+            this.Grade.TabIndex = 19;
+            // 
+            // FormCustomercs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 328);
+            this.Controls.Add(this.Grade);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Average);
+            this.Controls.Add(this.TextBoxval6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TextBoxval5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxval3);
+            this.Controls.Add(this.textBoxval2);
+            this.Controls.Add(this.GradeLabel);
+            this.Controls.Add(this.textBoxVal4);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.CheckButton);
+            this.Controls.Add(this.textBoxVal);
+            this.Controls.Add(this.LabelInput);
+            this.Name = "FormCustomercs";
+            this.Text = "FormCustomercs";
+            this.Load += new System.EventHandler(this.FormCustomercs_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelInput;
+        private System.Windows.Forms.TextBox textBoxVal;
+        private System.Windows.Forms.Button CheckButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.TextBox textBoxVal4;
+        private System.Windows.Forms.Label GradeLabel;
+        private System.Windows.Forms.TextBox textBoxval2;
+        private System.Windows.Forms.TextBox textBoxval3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TextBoxval5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TextBoxval6;
+        private System.Windows.Forms.TextBox Average;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Grade;
+    }
+}
---
+++ WindowsFormsApplication3/FormCustomercs.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class FormCustomercs : Form
+    {
+        public FormCustomercs()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            string grade1 = "D";  //1
+            string grade2 = "C";  //2
+            string grade3 = "B";  //3
+            string grade4 = "A";  //4
+            double value1 = Convert.ToDouble(textBoxVal.Text);   //5
+            double value2 = Convert.ToDouble(textBoxval2.Text);   //5
+            double value3 = Convert.ToDouble(textBoxval3.Text);   //5
+            double value4 = Convert.ToDouble(textBoxVal4.Text);   //5
+            double value5 = Convert.ToDouble(TextBoxval5.Text);   //5
+            double value6 = Convert.ToDouble(TextBoxval6.Text);   //5
+
+            
+                double average = (value1 + value2 + value3 + value4 + value5 + value6) / 6;
+                if (value1 >= 0 && value2 >= 0 && value3 >= 0 && value4 >= 0 && value5 >= 0 && value6 >= 0)
+                {                                      //7
+                    Average.Text = Convert.ToString(average); //8   
+                    if (average <= 49.9)   //9
+                        Grade.Text = "your grade is fail"; //10
+                    else if (average <= 67.9)    //11
+                        Grade.Text = "your grade is " + grade1;  //12
+                    else if (average <= 75.9) //13
+                        Grade.Text = "your grade is " + grade2;   //14
+                    else if (average <= 83.9) //15
+                        Grade.Text = "your grade is " + grade3;  //16
+                    else if (average <= 100)//17
+                        Grade.Text = "your grade is " + grade4;  //18
+                    //19
+                }
+                else
+                {
+                    MessageBox.Show("you should put only positive numbers"); //20
+                    textBoxVal.Clear();
+                    //21
+                }
+            }
+            catch (Exception) { 
+            MessageBox.Show("please fill the blanks");
+            }
+            
+        }//22
+        
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void FormCustomercs_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        }
+    }
+
